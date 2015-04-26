5e9234ae78228bbaffb7a51c077b2335991a123f
root <root@ebnymin2467.virt>
2015-04-27 02:01:00 +0300

    topick14 files

---
+++ init.pp
@@ -0,0 +1,28 @@
+lass jbapp {
+
+$deploy_path = '/opt/jboss-as-7.1.1.Final/standalone/deployments'
+
+exec {"temp_fold":
+ command => "/bin/mkdir -p /tmp/testweb",
+ creates => "/tmp/testweb",
+}
+
+exec{ 'get_app':
+ command => "/usr/bin/wget http://www.cumulogic.com/download/Apps/testweb.zip -O /tmp/testweb.zip",
+ creates => "/tmp/testweb.zip",
+}
+
+file { 'got_app':
+ mode => 0755,
+ path => "/tmp/testweb.zip",
+ require => Exec["get_app"],
+}
+
+exec {"unzip_app":
+ command => "/usr/bin/unzip /tmp/testweb.zip -d $deploy_path",
+
+}
+
+
+}
+
---
+++ mainsite.pp
@@ -0,0 +1,18 @@
+node 'default' {
+
+ include 'java'
+
+ class { 'jboss':
+  install             => 'source',
+  version             => '7',
+       }
+ jboss::instance {'app':
+        createuser => false,
+        bindaddr   => '127.0.0.1',
+        port       => '8080',
+                 }
+
+ include 'jboss'
+ include 'nginx'
+}
+
